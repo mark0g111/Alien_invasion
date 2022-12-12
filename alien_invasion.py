8bb449169ab92559422483a7a7351704a215c77b
mark0g111 <9154547225@mail.ru>
2022-12-12 15:06:21 +0300

    Update bullet max_count in AlienInvasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,9 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             self._update_screen()
 
     def _check_events(self):
@@ -52,8 +55,9 @@ class AlienInvasion:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
