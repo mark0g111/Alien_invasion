fa9e760472d2fbbe4d1024eee2b18cf44981cc68
mark0g111 <9154547225@mail.ru>
2022-12-12 10:17:58 +0300

    Create Ship class

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,7 +10,7 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
     def run_game(self):
